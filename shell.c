8ccea14c46d763df60d57b5075cf4e03b4ec5a79
mahdixabid <mahdixabid>
2021-04-15 21:37:02 +0100

    fixed betty

--- shell.c
+++ shell.c
@@ -1,47 +1,68 @@
 #include "shell.h"
-int _getline()
+/**
+ * _getline - function that used to read a string or a line
+ * Return: 0
+ */
+int _getline(void)
 {
 	char *string;
 	size_t size;
 	ssize_t bytes_read = 0;
 	int i;
-	string = (char *) malloc (size);
+
+	string = (char *)malloc(size);
 	if (bytes_read == -1)
-		{
-			puts ("hsh");
-		}
+	{
+		puts("hsh");
+	}
 	else
 		for (i = 0; i < 1; i++)
-			{
-				printf ("$ " );
-				bytes_read = getline (&string, &size, stdin);
-			}
+		{
+			printf("$ ");
+			bytes_read = getline(&string, &size, stdin);
+		}
 	return (0);
 }
-int _strtok()
+/**
+ * _strtok - function that used to read a string or a line
+ * Return: 0
+ */
+int _strtok(void)
 {
 	char *str = " ";
 	char *token = strtok(str, " ");
+
 	while (token != NULL)
-		{
-			printf("%s", token);
-			token = strtok(NULL, " ");
-		}
+	{
+		printf("%s", token);
+		token = strtok(NULL, " ");
+	}
 	return (0);
 }
-void _fork()
-{    
+/**
+* _fork - creating a new process
+*Return: void
+*/
+void _fork(void)
+{
 	char *argv[] = {"/bin/ls", "-l", ".", NULL};
+
 	pid_t pid = fork();
+
 	if (pid == 0)
 	{
-		execve(argv[0], argv, NULL);	
+		execve(argv[0], argv, NULL);
 	}
 	else
 	{
-		wait (NULL);
+		wait(NULL);
 	}
 }
+/**
+* sig_handler - checks if Ctrl C is pressed by the input
+* @signum: int
+*Return: void
+*/
 void sig_handler(int signum)
 {
 
@@ -50,15 +71,20 @@ void sig_handler(int signum)
 		write(STDOUT_FILENO, "\n$ ", 3);
 	}
 }
+/**
+ *main - main function
+ *Return: Always(0)
+ */
 int main(void)
 {
 	int st = 1;
+
 	signal(SIGINT, sig_handler);
-	while(st)
+	while (st)
 		{
 		_getline();
 		_strtok();
 		_fork();
-		}     
+		}
 	return (0);
 }
